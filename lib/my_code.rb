0e7dc74a1d8895a049912329918ac9026a3b8906
Aaron Blum <blumaa@gmail.com>
2019-07-30 16:53:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  x = 0
+  while x < array.length do
+    new_array << yield(array[x])
+    x += 1
+  end
+  new_array
+end
+
+def reduce(array)
+  accum = 0
+  
+end
